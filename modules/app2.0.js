ed981a4f0048566ac0071093ee2d246b28e0fb09
eduardo343 <54459451+eduardo343@users.noreply.github.com>
2021-03-10 17:44:53 -0600

    Update app2.0.js

--- modules/app2.0.js
+++ modules/app2.0.js
@@ -1,1 +1,13 @@
-let obj = {};
+let persona = { 
+nombre: "alan",
+apellido: "saucedo"
+getName: function (){
+    return `${this.nombre} ${this.apellido}`
+},
+domicilio: {
+    calle: "ramon bentacourt #426",
+    colonia: "centro",
+    cp: 2898
+    municipio: "colima"
+}
+console.log(persona.domicilio);
